4db98e43a2dd970cdb7dd3a1ee81c63f27629f27
nekoimi <nekoimime@gmail.com>
2024-09-10 00:59:01 +0800

    修改构建脚本

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # cuda-ollama-devel-container
 
 - ubuntu22.04
+- python3.10.12
 - cuda:11.8.0
 - ollama:latest
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
 
+### 切换国内源
+echo '切换国内源...'
+cp /etc/apt/sources.list /etc/apt/sources.list.back
+cat > /etc/apt/sources.list <<EOF
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+
+deb https://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+deb-src https://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+EOF
+
 ### 修改root密码
 echo "root:$ROOT_PASSWORD" | chpasswd
 
 ### 启动ssh
+echo '启动ssh...'
 service ssh start
 
 ### 启动docker
+echo '启动docker...'
 service docker start
 
 ### 执行CMD参数
